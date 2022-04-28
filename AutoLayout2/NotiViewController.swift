2ecee4437e13a67c8018755fd175fd66e1476fe7
MacOne-YJ4KBJ <macone-yj4kbj@MacOne-YJ4KBJs-MacBook-Pro.local>
2022-04-28 15:10:36 +0700

    first commit

---
+++ AutoLayout2/NotiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotiViewController.swift
+//  AutoLayout2
+//
+//  Created by MacOne-YJ4KBJ on 4/28/22.
+//
+
+import UIKit
+
+class NotiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
